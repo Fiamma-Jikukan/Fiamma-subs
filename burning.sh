4be759af7ee46053046897bad6c4582d58ad5360
Fiamma <stavtxxx1@gmail.com>
2023-03-17 12:15:40 +0200

    improving subs archive

--- burning.sh
+++ burning.sh
@@ -3,10 +3,10 @@ video="$1"
 OUTPUT="$2"
 
 
-ffmpeg -i "$video (1).mkv" -vf "ass='$video (1).ass'" "$OUTPUT" 
+ffmpeg -i "$video (1).mkv" -vf "ass='$video (1).ass'" "$OUTPUT Burned.mp4" 
 
 rm "$video (1).mkv"
 
-cp "$video (1).ass" ~/My\ Drive/Fiamma\ subs/no\ power/
+cp "$video (1).ass" ~/My\ Drive/Fiamma\ subs/no\ power/"$OUTPUT .ass"
 
 rm "$video (1).ass"
